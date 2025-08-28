30efb135a768cf5eeafab5162ed3a1e3cbdd324b
Alpana <km.alpana@cern.ch>
2025-08-28 16:00:09 +0200

    adding instructions to run the scripts

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # WR_plottingscripts
 Scripts to plot different variables in respect to WR analysis
+
+
+1. Overlay of ee CR and SR in DY and TTbar
+```
+python3 diffKinem_CRvsSR_rooFit_plot.py data/inputfiles/WRAnalyzer_DYJets.root data/jsons/hists.json
+```
+OR
+```
+python3 diffKinem_CRvsSR_rooFit_plot.py data/inputfiles/WRAnalyzer_TTbar.root data/jsons/hists_ttbar.json
+```
+
+2. Overlay of mumu CR and SR in DY and TTbar
+```
+python3 diffKinem_CRvsSR_rooFit_plot.py data/inputfiles/WRAnalyzer_DYJets.root data/jsons/hists.json   
+```
+
+```
+python3 diffKinem_CRvsSR_rooFit_plot.py data/inputfiles/WRAnalyzer_TTbar.root data/jsons/hists_mumu_ttbar.json
+```
+
+3. Overlay of different kinematics for different Wr masses and Nl masses
+```
+python3 v1_signal_diffkinem.py data/inputfiles/WRAnalyzer_signal_WR2000_N400.root data/inputfiles/WRAnalyzer_signal_WR2000_N800.root  data/inputfiles/WRAnalyzer_signal_WR2000_N1900.root data/jsons/hists_signal.json
+
+```
+Different Nl
+```
+python3 v1_signal_diffkinem.py data/inputfiles/WRAnalyzer_signal_WR2000_N800.root data/inputfiles/WRAnalyzer_signal_WR3200_N800.root  data/inputfiles/WRAnalyzer_signal_WR1200_N800.root data/jsons/hists_signal.json
+
+```
+4. Invariant mlljj - fit it using Gaussian
+```
+python3 rooFit_plot_Wrmass_BW.py WRAnalyzer_signal_WR3200_N1200.root
+```
+
+5. Or run all of it together
+```
+source runme.sh
+```
--- runme.sh
+++ runme.sh
@@ -1,12 +1,15 @@
 ## SR versus CR plots for TTBar and DY background
-python3 diffKinem_CRvsSR_rooFit_plot.py WRAnalyzer_DYJets.root hists_mumu.json
-python3 diffKinem_CRvsSR_rooFit_plot.py WRAnalyzer_DYJets.root hists.json ## for ee channel
+python3 diffKinem_CRvsSR_rooFit_plot.py data/inputfiles/WRAnalyzer_DYJets.root data/jsons/hists_mumu.json
+python3 diffKinem_CRvsSR_rooFit_plot.py data/inputfiles/WRAnalyzer_DYJets.root data/jsons/hists.json ## for ee channel
 
-python3 diffKinem_CRvsSR_rooFit_plot.py WRAnalyzer_TTbar.root hists_ttbar.json
-python3 diffKinem_CRvsSR_rooFit_plot.py WRAnalyzer_TTbar.root hists_mumu_ttbar.json
+python3 diffKinem_CRvsSR_rooFit_plot.py data/inputfiles/WRAnalyzer_TTbar.root data/jsons/hists_ttbar.json
+python3 diffKinem_CRvsSR_rooFit_plot.py data/inputfiles/WRAnalyzer_TTbar.root data/jsons/hists_mumu_ttbar.json
 
 
 ## overlay for signal
-python3 v1_signal_diffkinem.py WRAnalyzer_signal_WR2000_N400.root WRAnalyzer_signal_WR2000_N1000.root  WRAnalyzer_signal_WR2000_N1900.root hists_signal.json
+python3 v1_signal_diffkinem.py data/inputfiles/WRAnalyzer_signal_WR2000_N400.root data/inputfiles/WRAnalyzer_signal_WR2000_N800.root  data/inputfiles/WRAnalyzer_signal_WR2000_N1900.root data/jsons/hists_signal.json
 
-python3 v1_signal_diffkinem.py WRAnalyzer_signal_WR2000_N1000.root WRAnalyzer_signal_WR3200_N800.root  WRAnalyzer_signal_WR2800_N600.root hists_signal.json
+python3 v1_signal_diffkinem.py data/inputfiles/WRAnalyzer_signal_WR2000_N800.root data/inputfiles/WRAnalyzer_signal_WR3200_N800.root  data/inputfiles/WRAnalyzer_signal_WR1200_N800.root data/jsons/hists_signal.json
+
+
+python3 rooFit_plot_Wrmass_BW.py data/inputfiles/WRAnalyzer_signal_WR3200_N800.root
